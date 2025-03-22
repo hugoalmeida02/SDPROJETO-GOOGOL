2e30995320e5e23d7891cf62380d5c11d2606c16
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-03-22 21:25:57 +0000

    Implement queue able to recover from crash

--- search/url_queue.py
+++ search/url_queue.py
@@ -4,7 +4,6 @@ import grpc
 from google.protobuf import empty_pb2
 from concurrent import futures
 import argparse
-import queue
 import json
 import os
 import threading
@@ -12,37 +11,80 @@ import time
 
 SAVE_INTERVAL = 5
 
-
 class QueueService(index_pb2_grpc.IndexServicer):
-    def __init__(self):
-        self.host = "localhost"
-        self.port = "8180"
-        self.queue = queue.Queue()
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.url_queue_file = f"url_queue_{self.host}_{self.port}.json"
+        self.queue = []
+        self.lock = threading.Lock()
+        self.load_data()
+        self.auto_save_thread = threading.Thread(target=self.auto_save, daemon=True).start()
+
+    def load_data(self):
+        """Carrega os dados do ficheiro JSON ou cria ficheiro vazio se não existir."""
+        # Carregar dados do ficheiro de index, ou criar se não existir
+        if os.path.exists(self.url_queue_file):
+            with open(self.url_queue_file, "r") as f:
+                self.queue = json.load(f)
+            print(f"✅ Dados carregados do ficheiro {self.url_queue_file}")
+        else:
+            # Se o ficheiro não existir, cria um ficheiro vazio
+            with open(self.url_queue_file, "w") as f:
+                json.dump({}, f)
+            print(
+                f"⚠️ Ficheiro {self.url_queue_file} não encontrado. Criado um novo ficheiro vazio.")
+            self.queue = []
+    
+    def save_data(self):
+        """Guarda os dados no ficheiro JSON."""
+        with self.lock:
+            with open(self.url_queue_file, "w") as f:
+                json.dump(self.queue, f, indent=2)
+
+    def auto_save(self):
+        """Guarda periodicamente os dados em ficheiros JSON."""
+        while True:
+            time.sleep(SAVE_INTERVAL)
+            self.save_data()
     
     def takeNext(self, request, context):
-        url = self.queue.get()
-        return index_pb2.TakeNextResponse(url=url)
+        with self.lock:
+            if self.queue:
+                url = self.queue.pop(0)
+                return index_pb2.TakeNextResponse(url=url)
+            else:
+                return index_pb2.TakeNextResponse(url="")
 
     def putNew(self, request, context):
-        self.queue.put(request.url)
-        print(f"Added URL to index: {request.url}")
+        with self.lock:
+            self.queue.append(request.url)
+            print(f"Added URL to index: {request.url}")
         return empty_pb2.Empty()
 
 
-def queue_runner():
+def run(host, port):
     try:
-        queue1 = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        queue_service = QueueService()
-        index_pb2_grpc.add_IndexServicer_to_server(queue_service, queue1)
+        queue = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        queue_service = QueueService(host, port)
+        index_pb2_grpc.add_IndexServicer_to_server(queue_service, queue)
 
-        queue1.add_insecure_port("[::]:8180")  # O Gateway escuta na porta 8190
-        queue1.start()
-        print(f"Queue RPC iniciado na porta 8180")
-        queue1.wait_for_termination()
+        queue.add_insecure_port(f"{host}:{port}")  # O Gateway escuta na porta 8190
+        queue.start()
+        print(f"Queue RPC started on {host}:{port}")
+        queue.wait_for_termination()
     except KeyboardInterrupt:
         print("\nStopping the Url Queue...")
         return
 
 
 if __name__ == "__main__":
-    queue_runner()
+    parser = argparse.ArgumentParser(description="Url Queue")
+    parser.add_argument("--host", type=str, required=True,
+                        help="Host para o servidor gRPC")
+    parser.add_argument("--port", type=str, required=True,
+                        help="Porta para o servidor gRPC")
+    
+    args = parser.parse_args()
+    
+    run(args.host, args.port)
