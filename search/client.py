2496bbe5cca2925088b9b860218d9eb373f99c39
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-03-23 16:07:01 +0000

    Update

--- search/client.py
+++ search/client.py
@@ -5,37 +5,41 @@ from google.protobuf import empty_pb2
 import time
 import argparse
 
+
 def run(host_gateway, port_gateway):
     channel = grpc.insecure_channel(f"{host_gateway}:{port_gateway}")
     stub = index_pb2_grpc.IndexStub(channel)
-    
+
     print("##### Menu Client #####")
     print("1 - Adicionar Url à Queue")
     print("2 - Pesquisar palavras")
     print("3 - Pesquisar backlinks")
     print("4 - Consultar estatísticas")
     print("5 - Sair")
-                
+
     while True:
         try:
             try:
                 print("Escolhe uma opcão de 1 a 5")
                 option = input()
-                
+
                 if option == "1":
                     while True:
                         url = input()
                         if url.startswith("http"):
                             stub.putNew(index_pb2.PutNewRequest(url=url))
                             break
-                        else: 
+                        else:
                             print("Url inválido! Tenta novamente")
                 elif option == "2":
                     words = input()
-                    response = stub.searchWord(index_pb2.SearchWordRequest(words=words))
+                    response = stub.searchWord(
+                        index_pb2.SearchWordRequest(words=words))
                     if response.urls:
-                        print(f"A pesquisa '{words}' foi encontrada nos seguintes URLs:")
-                        paginas = [response.urls[i:i + 10] for i in range(0, len(response.urls), 10)]
+                        print(
+                            f"A pesquisa '{words}' foi encontrada nos seguintes URLs:")
+                        paginas = [response.urls[i:i + 10]
+                                   for i in range(0, len(response.urls), 10)]
                         for i, pagina in enumerate(paginas):
                             print(f"Página {i+1}:")
                             for urls in pagina:
@@ -50,11 +54,13 @@ def run(host_gateway, port_gateway):
                         url = input()
                         if url.startswith("http"):
                             break
-                        else: 
+                        else:
                             print("Url inválido! Tenta novamente")
-                    response = stub.searchBacklinks(index_pb2.SearchBacklinksRequest(url=url))
+                    response = stub.searchBacklinks(
+                        index_pb2.SearchBacklinksRequest(url=url))
                     if response.backlinks:
-                        print(f"A url '{url}' foi encontrada nos seguintes URLs:")
+                        print(
+                            f"A url '{url}' foi encontrada nos seguintes URLs:")
                         for backlinks in response.backlinks:
                             print(f" Url - {backlinks}")
                     else:
@@ -68,7 +74,8 @@ def run(host_gateway, port_gateway):
                     if response.barrels:
                         print("\nIndex Barrels ativos:")
                         for barrel in response.barrels:
-                            print(f"{barrel.address} — Entradas: {barrel.index_size} — {barrel.avg_response_time:.3f} décimas")
+                            print(
+                                f"{barrel.address} — Entradas: {barrel.index_size} — {barrel.avg_response_time:.3f} décimas")
                     else:
                         print("\nNenhum Index Barrels ativo.")
                 elif option == "5":
@@ -80,7 +87,8 @@ def run(host_gateway, port_gateway):
                 if e.details().startswith("WSASend: Connection reset"):
                     time.sleep(2)
                     print("Falha na ligação tente novamente")
-                    channel = grpc.insecure_channel(f"{host_gateway}:{port_gateway}")
+                    channel = grpc.insecure_channel(
+                        f"{host_gateway}:{port_gateway}")
                     stub = index_pb2_grpc.IndexStub(channel)
                 else:
                     print(f"RPC failed: {e.code()}")
@@ -89,6 +97,7 @@ def run(host_gateway, port_gateway):
             print("\nStopping the client...")
             return
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Client")
     parser.add_argument("--host_gateway", type=str, required=True,
@@ -96,5 +105,5 @@ if __name__ == "__main__":
     parser.add_argument("--port_gateway", type=str, required=True,
                         help="Porta para a gateway gRPC")
     args = parser.parse_args()
-    
+
     run(args.host_gateway, args.port_gateway)
--- search/downloader.py
+++ search/downloader.py
@@ -14,66 +14,75 @@ def extract_info(html, base_url):
     """ Extrai a informação de uma página """
     soup = jsoup(html, 'html.parser')
     links = set()
-    
+
     for a in soup.find_all('a', href=True):
         href = a['href']
         if href.startswith('http'):
             links.add(href)
-            
+
     text = soup.get_text()
     words = re.findall(r'\b\w{3,}\b', text.lower())  # Palavras com 3+ letras
-    
+
     title = soup.title.string if soup.title else 'Título não encontrado'
     quote = soup.find('p').text if soup.find('p') else 'Citação não encontrada'
-    
+
     return set(words), links, title, quote
 
+
 def run(host_gateway, port_gateway, host_url_queue, port_url_queue):
-    url_queue_channel = grpc.insecure_channel(f"{host_url_queue}:{port_url_queue}")  # Conectar à queue
+    url_queue_channel = grpc.insecure_channel(
+        f"{host_url_queue}:{port_url_queue}")  # Conectar à queue
     url_queue_stub = index_pb2_grpc.IndexStub(url_queue_channel)
-    gateway_channel = grpc.insecure_channel(f"{host_gateway}:{port_gateway}")  # Conectar à gateway
+    gateway_channel = grpc.insecure_channel(
+        f"{host_gateway}:{port_gateway}")  # Conectar à gateway
     gateway_stub = index_pb2_grpc.IndexStub(gateway_channel)
 
     while True:
         try:
             try:
-                response = url_queue_stub.takeNext(empty_pb2.Empty())  # Pede um URL da fila
+                response = url_queue_stub.takeNext(
+                    empty_pb2.Empty())  # Pede um URL da fila
             except:
                 time.sleep(2)
                 print("Tentando reconectar à url_queue")
-                url_queue_channel = grpc.insecure_channel(f"{host_url_queue}:{port_url_queue}")  # Conectar à queue
+                url_queue_channel = grpc.insecure_channel(
+                    f"{host_url_queue}:{port_url_queue}")  # Conectar à queue
                 url_queue_stub = index_pb2_grpc.IndexStub(url_queue_channel)
                 continue
-            
+
             url = response.url
             print(f"Processando URL: {url}")
-            
+
             if not url:
                 continue
-            
+
             try:
                 # Fetch webpage using requests and parse with BeautifulSoup
                 response = requests.get(url)
                 response.raise_for_status()  # Raise an exception for bad status codes
-                
+
                 words, links, title, quote = extract_info(response.text, url)
-                
+
                 while True:
                     try:
-                        response = gateway_stub.getIndexBarrels(empty_pb2.Empty())
+                        response = gateway_stub.getIndexBarrels(
+                            empty_pb2.Empty())
                     except:
                         time.sleep(2)
                         print("Tentando reconectar à gateway")
-                        gateway_channel = grpc.insecure_channel(f"{host_gateway}:{port_gateway}")  # Conectar à gateway
-                        gateway_stub = index_pb2_grpc.IndexStub(gateway_channel)
+                        gateway_channel = grpc.insecure_channel(
+                            # Conectar à gateway
+                            f"{host_gateway}:{port_gateway}")
+                        gateway_stub = index_pb2_grpc.IndexStub(
+                            gateway_channel)
                         continue
-                    
-                    SERVERS = [] #Guarda os Index Barrels ativos
-                    
+
+                    SERVERS = []  # Guarda os Index Barrels ativos
+
                     for server in response.indexInfos:
                         if server not in SERVERS:
                             SERVERS.append(server)
-                            
+
                     if len(SERVERS) != 0:
                         server_index = random.randint(0, len(SERVERS) - 1)
 
@@ -82,30 +91,34 @@ def run(host_gateway, port_gateway, host_url_queue, port_url_queue):
                             with grpc.insecure_channel(server) as channel:
                                 print(f"Connectado server {server}")
                                 index_stub = index_pb2_grpc.IndexStub(channel)
-                                
+
                                 for word in words:
-                                    index_stub.addToIndexWords(index_pb2.AddToIndexRequestWords(word=word, url=url, from_multicast=False))
-                                    
-                                for link in links:   
-                                    index_stub.addToIndexLinks(index_pb2.AddToIndexRequestLinks(url=url, title=title, quote=quote,  link=link, from_multicast=False))
-                                
-                            for link in links:   
-                                url_queue_stub.putNew(index_pb2.PutNewRequest(url=link))
-                                    
+                                    index_stub.addToIndexWords(index_pb2.AddToIndexRequestWords(
+                                        word=word, url=url, from_multicast=False))
+
+                                for link in links:
+                                    index_stub.addToIndexLinks(index_pb2.AddToIndexRequestLinks(
+                                        url=url, title=title, quote=quote,  link=link, from_multicast=False))
+
+                            for link in links:
+                                url_queue_stub.putNew(
+                                    index_pb2.PutNewRequest(url=link))
+
                         except grpc.RpcError as e:
                             print(f"Erro server {server}")
-                    
+
                         break
                     else:
                         print("Nenhum Index Barrel disponivel")
-                
+
             except requests.RequestException as e:
                 print(f"Error fetching webpage: {e}")
-                 
+
         except KeyboardInterrupt:
             print("\nStopping the robot...")
             return
-        
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Downloader")
     parser.add_argument("--host_gateway", type=str, required=True,
@@ -117,5 +130,6 @@ if __name__ == '__main__':
     parser.add_argument("--port_url_queue", type=str, required=True,
                         help="Porta para a url_queue gRPC")
     args = parser.parse_args()
-    
-    run(args.host_gateway, args.port_gateway, args.host_url_queue, args.port_url_queue)
+
+    run(args.host_gateway, args.port_gateway,
+        args.host_url_queue, args.port_url_queue)
